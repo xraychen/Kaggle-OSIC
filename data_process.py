ed41e74d5aa4a6d93344389adf6cf04d98f99b90
xray <b06901135@ntu.edu.tw>
2020-07-21 16:29:13 +0800

    backup

--- data_process.py
+++ data_process.py
@@ -3,14 +3,15 @@ import pydicom
 import numpy as np
 
 import matplotlib.pyplot as plt
-from pydicom.pixel_data_handlers.util import apply_color_lut
 
 
 def make_dir(file_path):
     dirname = os.path.dirname(file_path)
     try:
-        if not os.path.exists(dirname): os.mkdir(dirname)
-    except FileNotFoundError: pass
+        if not os.path.exists(dirname):
+            os.mkdir(dirname)
+    except FileNotFoundError:
+        pass
 
 
 def plot_images(images, output_file):
@@ -26,26 +27,43 @@ def plot_images(images, output_file):
 
 
 def onehot(idx, length):
-    temp = np.zeros((length), np.uint8)
+    temp = np.zeros((length), np.float32)
     temp[idx] = 1
     return temp
 
 
 def to_onehot(value, category):
-    if (category == 'age'):
+    if (category == 'week'):
+        idx = int(value) - Y_OFFSET
+        length = Y_LENGTH
+    elif (category == 'age'):
         idx = (int(value) - 50) // 5
         idx = np.clip(idx, 0, 7)
         length = 8
     elif (category == 'sex'):
         idx = ['Male', 'Female'].index(value)
         length = 2
-    elif (category == 'smoke_status'):
+    elif (category == 'smoking_status'):
         idx = ['Never smoked', 'Ex-smoker', 'Currently smokes'].index(value)
         length = 3
     else:
         raise KeyError
 
     return onehot(idx, length)
+
+
+def codec_fcv(value, decode=False):
+    if decode:
+        return float(value) * 4000.
+    else:
+        return float(value) / 4000.
+
+
+def codec_percent(value, decode=False):
+    if decode:
+        return float(value) * 100.
+    else:
+        return float(value) / 100.
 
 
 def normalize(pixel_array, image_size):
@@ -57,90 +75,78 @@ def normalize(pixel_array, image_size):
     return pixel_array
 
 
-# TODO fix gdcm dependencies
-def load_training_data(train_dir, train_csv, output_dir, limit_num=50, image_size=512):
-    users_id = sorted(os.listdir(train_dir))
-    # pad_image = np.zeros((image_size, image_size), np.uint8)
-    pad_image = np.zeros((image_size, image_size, 3), np.uint8)
-
-    # train_images = np.zeros((len(users_id), limit_num, image_size, image_size), np.uint8)
-    train_images = np.zeros((len(users_id), limit_num, image_size, image_size, 3), np.uint8)
-    train_x = np.zeros((len(users_id), 13), np.uint8)
-    train_y = np.zeros((len(users_id), 146), np.int16)
-
-    with open(train_csv) as f:
-        content = f.read().splitlines()
+def process_data(csv_file, image_dir, output_dir, limit_num=20, image_size=256):
+    with open(csv_file) as f:
+        content = f.read().splitlines()[1:]
         content = [e.split(',') for e in content]
 
-    for i, user_id in enumerate(users_id):
-        # train_images
-        image_arr = os.listdir(os.path.join(train_dir, user_id))
-        image_arr = [e.split('.')[0] for e in image_arr]
-        image_arr = sorted([int(e) for e in image_arr])
+    x = np.zeros((len(content), X_LENGTH), np.float32)
+    y = np.zeros((len(content), Y_LENGTH), np.float32)
 
-        for j in range(limit_num):
-            if j < len(image_arr):
-                try:
-                    image = pydicom.dcmread(os.path.join(train_dir, user_id, '{}.dcm'.format(image_arr[j])))
+    cache_user_id = None
+    cache_y = np.zeros((Y_LENGTH), np.float32)
+    cache_image = np.zeros((limit_num, image_size, image_size), np.uint8)
 
+    empty_image = np.zeros((image_size, image_size), np.uint8)
+    images = []
 
+    images_id = np.zeros((len(content)), np.int16)
 
-
-                    pixel_array = apply_color_lut(image.pixel_array, palette='HOT_IRON')
-                    pixel_array = cv2.resize(pixel_array, (image_size, image_size))
-                    train_images[i, j, :, :, :] = pixel_array
-                    # train_images[i, j, :, :] = normalize(image.pixel_array, image_size)
-                except RuntimeError:
-                    # train_images[i, j, :, :] = pad_image
-                    train_images[i, j, :, :, :] = pad_image
-            else:
-                # train_images[i, j, :, :] = pad_image
-                train_images[i, j, :, :, :] = pad_image
-
-        # trian_x
-        temp = list(filter(lambda x: x[0] == user_id, content))
-
-        train_x[i, :] = np.concatenate((
-            to_onehot(temp[0][4], 'age'),
-            to_onehot(temp[0][5], 'sex'),
-            to_onehot(temp[0][6], 'smoke_status')
+    for i, line in enumerate(content):
+        # generate x
+        x[i, :] = np.concatenate((
+            to_onehot(line[1], 'week'),
+            np.array([codec_fcv(line[2]), codec_percent(line[3])], np.float32),
+            to_onehot(line[4], 'age'),
+            to_onehot(line[5], 'sex'),
+            to_onehot(line[6], 'smoking_status')
         ), axis=0)
 
-        # train_y
-        p = 0
-        for j in range(146):
-            if j - 12 > int(temp[p][1]) and p < len(temp) - 1:
-                p += 1
-            train_y[i, j] = int(temp[p][2])
+        user_id = line[0]
+        if cache_user_id != user_id:
+            cache_user_id = user_id
+            # generate y
+            user_filter = list(filter(lambda x: x[0] == user_id, content))
+            p = 0
+            for j in range(Y_LENGTH):
+                if j + Y_OFFSET > int(user_filter[p][1]) and p < len(user_filter) - 1:
+                    p += 1
+                cache_y[j] = codec_fcv(user_filter[p][2])
 
-    plot_images(train_images[:, 0, :, :], 'plot/sample_hot_iron.png')
-    # print(np.mean(train_y), np.std(train_y))
+            # generate image
+            image_arr = os.listdir(os.path.join(image_dir, user_id))
+            image_arr = [e.split('.')[0] for e in image_arr]
+            image_arr = sorted([int(e) for e in image_arr])
 
-    if not os.path.exists(output_dir):
-        os.mkdir(output_dir)
+            for j in range(limit_num):
+                if j < len(image_arr):
+                    try:
+                        image = pydicom.dcmread(os.path.join(image_dir, user_id, '{}.dcm'.format(image_arr[j])))
+                        cache_image[j, :, :] = normalize(image.pixel_array, image_size)
+                    except RuntimeError:
+                        cache_image[j, :, :] = empty_image
+                else:
+                    cache_image[j, :, :] = empty_image
 
-    # np.save(os.path.join(output_dir, 'train_images.npy'), train_images)
-    # np.save(os.path.join(output_dir, 'train_x.npy'), train_x)
-    # np.save(os.path.join(output_dir, 'train_y.npy'), train_y)
+            images.append(cache_image)
+            image_id = len(images) - 1
 
+        y[i, :] = cache_y
+        images_id[i] = image_id
 
-def load_testing_data(test_dir, limit_num=50):
-    pass
+    images = np.array(images, np.uint8)
 
-
-def test():
-    image = pydicom.dcmread('raw/train/ID00007637202177411956430/1.dcm')
-    print(image)
-    return 0
-    pixel_array = image.pixel_array
-    print(pixel_array.dtype)
-    pixel_array = apply_color_lut(pixel_array, palette='HOT_IRON')
-    print(pixel_array.dtype)
-    plt.imshow(pixel_array)
-    plt.savefig('plot/_.png')
-    plt.close()
+    print(x.shape)
+    print(x[:5])
+    print(y.shape)
+    print(y[:5])
+    print(y[15:20])
+    print(images_id[:20])
+    print(images.shape)
 
 
 if __name__ == '__main__':
-    # load_training_data('raw/train', 'raw/train.csv', 'input/', limit_num=10, image_size=256)
-    test()
+    X_LENGTH = 161
+    Y_LENGTH = 146
+    Y_OFFSET = -12
+    process_data('raw/train.csv', 'raw/train', 'input/')
